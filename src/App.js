304e9e0c5835b4ec3f9956ac9d8ebd16ec9e3ad8
erfan aboei <erfan.a3840@gmail.com>
2023-02-18 15:34:26 +0330

    Create Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Home from './View/Home/home';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+        {Home()}
     </div>
   );
 }
